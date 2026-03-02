1221d6023eef07ce892cda4993bcdd2c3c4d0b92
tommyokeefe <tommyokeefe81@gmail.com>
2026-03-02 12:38:10 -0500

    chore(test): add cypress testing

---
+++ cypress/support/e2e.ts
@@ -0,0 +1,5 @@
+// Prevent Cypress from failing tests on uncaught exceptions
+// from third-party scripts (Vercel Analytics, Giscus, etc.)
+Cypress.on("uncaught:exception", (_err, _runnable) => {
+    return false;
+});
