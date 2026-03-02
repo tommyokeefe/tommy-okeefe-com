a64d86ba2e433366dd4c8caecffd046aa450452a
tommyokeefe <tommyokeefe81@gmail.com>
2026-03-02 14:29:01 -0500

    feat: add support for bluesky comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tommyokeefe.com",
-  integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  integrations: [tailwind(), sitemap(), mdx(), pagefind(), react()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
---
+++ src/components/BlueskyComments.tsx
@@ -0,0 +1,17 @@
+import "bluesky-comments/bluesky-comments.css";
+import { BlueskyComments } from "bluesky-comments";
+
+interface Props {
+    uri: string;
+}
+
+export default function BlueskyCommentsSection({ uri }: Props) {
+    return (
+        <div className="mt-12">
+            <BlueskyComments
+                uri={uri}
+                onEmpty={(details) => console.error('Failed to load comments:', details)}
+            />
+        </div>
+    );
+}
