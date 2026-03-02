844cf102a0acf12059d08bf70eb33d433708b77f
tommyokeefe <tommyokeefe81@gmail.com>
2026-03-02 17:38:04 -0500

    fix(images): use the frontmatter image helper from astro

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
@@ -12,7 +12,7 @@ const blog = defineCollection({
     book: z.string().optional(),
     chapter: z.string().optional(),
     tags: z.array(z.string()).optional(),
-    image: z.string().optional(),
+    image: image().optional(),
     imageAlt: z.string().optional(),
     blueskyPostUri: z.string().optional(),
   }),
